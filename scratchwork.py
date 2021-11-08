76a57e72159cb8dcf4bcb135983474aa21ef6cea
Aayu05-YT <85261822+Aayu05-YT@users.noreply.github.com>
2021-11-07 19:26:28 -0500

    Add files via upload

---
+++ scratchwork.py
@@ -0,0 +1,109 @@
+SCORE=0
+#calling first question
+print("Q1,Who is the current U.S. president?")
+print("A.Joe Biden")
+print("B.Mark Zuckerberg")
+print("C.Serena Williams")
+print("D.Adolf Hitler")
+#accepting answer
+Q1A=input("enter A,B,C,D IN CAPITAL")
+#Grading answer
+if Q1A=="A":
+    print("Good Job")
+    #calling Q2
+    print("Q2, When was the COronaVirusDisease2019 declared a pandemic, and international emergency?")
+    print("A. May 14, 2010")
+    print("B. March 11, 2020")
+    print("C. May 8, 1945")
+    print("D. November 9, 1989")
+    SCORE=SCORE+1
+    Q2A=input("enter A,B,C,D IN CAPITAL")
+   #grading question
+    if Q2A=="B":
+        print("Good job")
+        print("FACT: The Berlin wall fell on November 9,1989. It divided about 3.5 million people. It was divided between U.S.,France,and Great Britan to the west. And, the Soviet Union to the east ")
+        print("FACT: On may 8, 1945; The allied Powers had defeated Nazi Germany, and the Axis powers and called it VE day to stand for Victory in Europe day. This ended a 6 year long conflict that took the lives of millions, and left many more seperated ")
+       #Q3
+        print("Q3, what company recently got sued for making a social media platform for kids?" )
+        print("A. Apple")
+        print("B. Amazon")
+        print("C. Facebook")
+        print("D. Samsung")
+        Q3A=input("enter A,B,C,D IN CAPITAL")
+        if Q3A=="C":
+            print("Good Job")
+            print("FACT: Facebook tried to make social media for younger kids but got sued for psycological and mental problems")
+            #Q4
+            print("Q4, A terrorist group recently took control of a middle eastern country after a world power left it. Which one?")
+            print("ISIS-K,Australlia")
+            print("Donald Trump,Quatar")
+            print("Ebrahim Raisi,India")
+            print("Taliban,Afghanistan")
+            Q4A=input("enter A,B,C,D IN CAPITAL")
+            if Q4A=="D":
+                print("Good Job")
+                print("FACT:(wsj.com)The Taliban, an Islamist fundamentalist organization that ruled Afghanistan from 1996 until being toppled by the 2001 U.S. invasion, seized the Afghan capital Kabul on Aug. 15 after President Ashraf Ghani escaped the country, crowning a rapid offensive that started in May.")
+                print("Anybody else notice it was on the Indian Independence day? :( ")
+                #Q5
+                print("Q5, What sport star unfortunately passed away in 2020?")
+                print("A.Kobe Bryant")
+                print("B.Rohit Sharma")
+                print("C.Lionel Messi")
+                print("D.Shohei Ohtani")
+                Q5A=input("enter A,B,C,D IN CAPITAL")
+                if Q5A=="A":
+                    print("You're on a roll!")
+                    print("FACT: Kobe Bryant was an american proffessional basketball player who died in a helicopter crash on Jan. 26, 2021")
+                    #Q6
+                    print("Q6, What two cricket players recently broke a world record scoring 140 runs in a single inns.?")
+                    print("A.Babar Azam, Mohammed Rizwan")
+                    print("B. Rohit Sharma, K.L. Rahul")
+                    print("C.Jos Butler, A.B.Devilliers")
+                    print("D.Adam Zampa, Cristpher Columbus")
+                    Q6A=input("enter A,B,C,D IN CAPITAL")
+                    if Q6A=="B":
+                        print("ur so good at this!")
+                        print("FACT: Rohit Sharma and KL Rahul shared a 140-run partnership, India's biggest ever T20 partnership. Earlier, the record was held by Virender Sehwag and Gautam Gambhir, who scored 136 runs against England. ")
+                        #Q7
+                        print("Q7, what country recently left the E.U.?")
+                        print("A.France")
+                        print("B.Namibia")
+                        print("C.Sweeden")
+                        print("D. U.K.")
+                        Q7A=input("enter A,B,C,D IN CAPITAL")
+                        if Q7A=="D":
+                            print("AMAZING! Almost there.")
+                            print("FACT(Wiki):Brexit (/ˈbrɛksɪt, ˈbrɛɡzɪt/; a portmanteau of British exit was the withdrawal of the United Kingdom (UK) from the European Union (EU) at 23:00 GMT on 31 January 2020 (00:00 CET). ")
+                            #Q8
+                            print("Tensions between what two countriest are rising?")
+                            print("A.India, Austria")
+                            print("B.France, Brazil")
+                            print("C. U.S., Russia,China,North Korea")
+                            print("D.Ghana, Portugal")
+                            Q8A=input("enter A,B,C,D IN CAPITAL")
+                            if Q8A=="C":
+                                print("Ur a champ!")
+                                print("FACT: These countries are huge players in the global millitary arsenal and tensions are rising due to hacks, millitary and nuclear arsenal")
+                                #Q9
+                                print("These two reigions are in millitary chaos")
+                                print("A.Myanmar, India")
+                                print("B.Ethiopia, Tigray")
+                                print("C.Delhi, Hydrabad")
+                                print("D.Georgia, California")
+                                Q9A=input("enter A,B,C,D IN CAPITAL")
+                                if Q9A=="B":
+
+                                    def poll():
+                                        print("should I do this again?")
+                                    poll()
+                                
+
+
+
+
+
+
+
+
+
+
